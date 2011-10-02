b338fb3481175f0ea90600152952f1bec6f052df
John E. Vincent <lusis.org+github.com@gmail.com>
2011-10-02 02:33:50 -0400

    forgot init.rb

--- init.rb
+++ init.rb
@@ -1,7 +1,17 @@
 require './lib/noah-agent'
 
-@redis = Noah::Agent::Redis.supervise_as :redis, "127.0.0.1", 6379
-@broker = Noah::Agent::Broker.supervise_as :broker, "noah_broker"
-@watcher = Noah::Agent::WatchList.supervise_as :watchlist, "http://localhost:5678"
+@redis = NoahAgent::Redis.supervise_as :redis, "127.0.0.1", 6379
+@broker = NoahAgent::Broker.supervise_as :broker, "noah_broker"
+@watcher = NoahAgent::WatchList.supervise_as :watchlist, "http://127.0.0.1:5678"
 
+NoahAgent::Pool.supervise_as :dummy_pool, "dummy_pool", {:num_workers => 10 , :worker_class => NoahAgent::Dummy}
+NoahAgent::Pool.supervise_as :http_pool, "http_pool", {:num_workers => 10 , :worker_class => NoahAgent::Http}
 
+@dp = Celluloid::Actor[:dummy_pool]
+@hp = Celluloid::Actor[:http_pool]
+#500.times { @dp.notify_worker! "dummy://foobar", '{"status":"up"}' }
+
+#while true do
+#  puts "#{@dp.available_workers.size}|#{@dp.busy_workers.size}|#{@dp.backlog.size}"
+#  sleep 5
+#end
